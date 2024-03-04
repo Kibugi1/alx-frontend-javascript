6613703a14b427a61d6651e2113a97b5b532f2ec
Kibugi1 <njugunabrian843@gmail.com>
2024-03-04 21:54:35 +0000

    Lint correction

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -1,7 +1,7 @@
 export default function appendToEachArrayValue(array, appendString) {
   const newArray = [...array];
-  for (const [idx, value] of array.entries()) {
-    if (idx === array.length - 1) {
+  for (const [idx, value] of newArray.entries()) {
+    if (idx === newArray.length - 1) {
       newArray[idx] = appendString + value;
     }
   }
