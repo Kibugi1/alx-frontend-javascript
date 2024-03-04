50322705fa1886a4cc6462f03654e09eb17214ae
Kibugi1 <njugunabrian843@gmail.com>
2024-03-04 15:33:11 +0000

    Hoisting correction

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,9 +1,8 @@
 export default function taskBlock(trueOrFalse) {
-  let task = false;
-  let task2 = true;
+  var task = false;
+  var task2 = true;
   if (trueOrFalse) {
-     task = false;
-     task2 = true;
+
   }
 
   return [task, task2];
