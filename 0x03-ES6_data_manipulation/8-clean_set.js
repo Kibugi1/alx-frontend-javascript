9699b0ccc8f52ba26809395c8cf9915bbe576b34
Kibugi1 <njugunabrian843@gmail.com>
2024-03-13 13:21:42 +0000

    Task 8: ESLint fix

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -4,9 +4,9 @@ export default function cleanSet(set, startString) {
   }
   const valuesArray = Array.from(set);
   const displayValues = valuesArray
-        .filter(value => value.startsWith(startString))
-        .map(value => value.slice(startString.length));
+    .filter((value) => value.startsWith(startString))
+    .map((value) => value.slice(startString.length));
 
-    // Join the display values with "-"
-    return displayValues.join('-');
+  // Join the display values with "-"
+  return displayValues.join('-');
 }
